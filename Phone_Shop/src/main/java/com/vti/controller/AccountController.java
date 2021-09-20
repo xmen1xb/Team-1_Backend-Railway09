7fa40c9d05058ce3ca46be4116f14fce928101f6
Xuanteo <81322256+Xuanteo@users.noreply.github.com>
2021-09-21 02:11:30 +0700

    Loi import o class account

--- Phone_Shop/src/main/java/com/vti/controller/AccountController.java
+++ Phone_Shop/src/main/java/com/vti/controller/AccountController.java
@@ -12,10 +12,14 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vti.entity.Account;
+import com.vti.request.AccountRequest;
 import com.vti.response.AccountResponse;
 import com.vti.service.IAccountService;
 
