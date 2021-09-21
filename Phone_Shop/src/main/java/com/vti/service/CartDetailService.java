b93220fc670a859b7dd89e0e8132cacec0f7873b
pnguyen237 <phuongduc.nguyen@dxc.com>
2021-09-21 22:54:17 +0700

    fix bug duplicate CreateCartDetail By Phuong

--- Phone_Shop/src/main/java/com/vti/service/CartDetailService.java
+++ Phone_Shop/src/main/java/com/vti/service/CartDetailService.java
@@ -1,5 +1,7 @@
 package com.vti.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,51 +17,60 @@ import com.vti.repository.ICartRepository;
 import com.vti.repository.IProductRepository;
 
 @Service
-public class CartDetailService implements ICartDetailService{
-	
+public class CartDetailService implements ICartDetailService {
+
 	@Autowired
 	private ICartDetailRepository cartdetailRepo;
-	
+
 	@Autowired
 	private IProductRepository productRepo;
-	
+
 	@Autowired
 	private ICartRepository cartRepo;
-	
+
 	@Autowired
 	private IAccountRepository accountRepo;
 
 	@Override
 	public Page<CartDetail> getAllCartDetail(Pageable pageable) {
-		
+
 		return cartdetailRepo.findAll(pageable);
 	}
 
 	@Override
 	public void createCartDetail(int producId, int accountId) {
-		CartDetail cartDetail = new CartDetail();
+
 		Product product = productRepo.getById(producId);
+		List<CartDetail> listCartDetail = product.getListCartDetail();
+		for (CartDetail cartDetail : listCartDetail) {
+			if (cartDetail.getProduct().getProduct_id() == product.getProduct_id()) {
+				updateCartDetailUp(cartDetail.getCartdetail_id());
+				return;
+			} 		
+		}	
+		CartDetail cartDetail2 = new CartDetail();
 		Account account = accountRepo.getById(accountId);
 		int cartID = account.getCart().getCart_id();
-		
-		cartDetail.setPrice(product.getPrice());
-		cartDetail.setQuantity(1);
-		cartDetail.setCart(cartRepo.getById(cartID));
-		cartDetail.setProduct(product);
-		cartdetailRepo.save(cartDetail);
-				
-		updateCartUp(cartID, cartDetail);
+
+		cartDetail2.setPrice(product.getPrice());
+		cartDetail2.setQuantity(1);
+		cartDetail2.setCart(cartRepo.getById(cartID));
+		cartDetail2.setProduct(product);
+
+		cartdetailRepo.save(cartDetail2);
+
+		updateCartUp(cartID, cartDetail2);
 	}
 
 	@Override
 	public void deleteCartDetail(int id) {
 		CartDetail cartDetail = cartdetailRepo.getById(id);
 		int cartID = cartDetail.getCart().getCart_id();
-		
+
 		updateCartDown(cartID, cartDetail);
-				
+
 		cartdetailRepo.deleteById(id);
-			
+
 	}
 
 	@Override
@@ -70,35 +81,35 @@ public class CartDetailService implements ICartDetailService{
 		cartdetailRepo.save(cartDetail);
 		updateCartUp(cartID, cartDetail);
 	}
-	
+
 	@Override
 	public void updateCartDetailDown(int id) {
 		CartDetail cartDetail = cartdetailRepo.getById(id);
 		int cartID = cartDetail.getCart().getCart_id();
 		int quantity = cartDetail.getQuantity() - 1;
 		cartDetail.setQuantity(quantity);
-		
+
 		if (quantity == 0) {
 			deleteCartDetail(id);
-			
-		}else {
+
+		} else {
 			cartdetailRepo.save(cartDetail);
 			updateCartDown(cartID, cartDetail);
 		}
-			
+
 	}
-	
+
 	public void updateCartUp(int cartID, CartDetail cartDetail) {
 		Cart cart = cartRepo.getById(cartID);
 		cart.setQuantity(cart.getQuantity() + 1);
-		cart.setTotal_price(cart.getTotal_price() + cartDetail.getPrice());	
+		cart.setTotal_price(cart.getTotal_price() + cartDetail.getPrice());
 		cartRepo.save(cart);
 	}
-	
+
 	public void updateCartDown(int cartID, CartDetail cartDetail) {
 		Cart cart = cartRepo.getById(cartID);
 		cart.setQuantity(cart.getQuantity() - 1);
-		cart.setTotal_price(cart.getTotal_price() - cartDetail.getPrice());	
+		cart.setTotal_price(cart.getTotal_price() - cartDetail.getPrice());
 		cartRepo.save(cart);
 	}
 }
